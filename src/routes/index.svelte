617ffc98fd6677a0b8cd74530577fd602ad23b0a
Antony Jones <aj@desirableobjects.co.uk>
2019-07-05 16:16:25 +0100

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<h1>
+	Page One.
+</h1>
+
+<button on:click={addParam}>add state with query</button>
+
+<button on:click={gotoAbout}>go to about page</button>
+
+<script>
+	import { goto } from '@sapper/app'
+
+	function gotoAbout () {
+		goto('/about')
+	}
+
+	function addParam () {
+		history.pushState(null, null, '/?foo=bar')
+	}
+</script>
